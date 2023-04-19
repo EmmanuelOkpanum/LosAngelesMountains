391c025284fd511e18412c7144e218c1f6b5fc2b
Engr Ebuka <eservices024.com>
2023-04-19 14:16:45 +0100

    Third update

--- src/Home/Thirdsection.js
+++ src/Home/Thirdsection.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 
 import AOS from "aos";
-// import "aos/dist/aos.css";
+import "aos/dist/aos.css";
 
 const Thirdsection = () => {
   const [activeTab, setActiveTab] = useState("mountain1");
